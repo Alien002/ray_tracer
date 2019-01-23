0fbb3dc66e1d52b58dd0f1edcdf04ffb20cc68ac
Alic Lien <Alien002@ucr.edu>
2019-01-22 22:50:36 -0800

    fixed segfault 1

--- sphere.cpp
+++ sphere.cpp
@@ -73,7 +73,7 @@ Hit Sphere::Intersection(const Ray& ray, int part) const
 }
 
 vec3 Sphere::Normal(const vec3& point, int part) const
-{   //std::cout<<"sphere normal called!!!!!!" <<std::endl;
+{   std::cout<<"sphere normal called!!!!!!" <<std::endl;
 
     vec3 normal;
     TODO; // compute the normal direction
