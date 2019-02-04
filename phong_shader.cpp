267a9095b4b7d3ead380b167ae92aef384886b21
Alic Lien <Alien002@ucr.edu>
2019-02-03 20:39:40 -0800

    Ray diffuse fix

--- phong_shader.cpp
+++ phong_shader.cpp
@@ -35,7 +35,7 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
         shadowray.direction = (current -> position-intersection_point).normalized();
         
         if(world.Closest_Intersection(shadowray).object == nullptr || (current - intersection_point < world.Closest_Intersection(shadowray).dist)){
-            Id = color_diffuse * light.Emitted_Light(current -> position) * max(dot(normal, intersection_point - light.Emitted_Light(current -> position)),0);
+            Id = color_diffuse * Emitted_Light(current -> position) * max(dot(normal, intersection_point - light.Emitted_Light(current -> position)),0);
             //Is = color_specular *
         }
     }
